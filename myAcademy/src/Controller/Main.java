36c354c377c67fe273395535cbc64dfc30953942
cl0ud7 <cl0ud7@cl0ud7-GE60-2OC-2OD-2OE>
2019-12-31 21:14:08 +0100

    Inicio

---
+++ myAcademy/src/Controller/Main.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        //Pane root = FXMLLoader.load(getClass().getResource("/application/view/Lay.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 640.0, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
