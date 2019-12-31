3cc529cd042398fc926194d1deee253df6a086b9
cl0ud7 <cl0ud7@cl0ud7-GE60-2OC-2OD-2OE>
2019-12-31 21:16:20 +0100

    Prueba

--- myAcademy/src/Controller/Main.java
+++ myAcademy/src/Controller/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         //Pane root = FXMLLoader.load(getClass().getResource("/application/view/Lay.fxml"));
-        primaryStage.setTitle("Login");
+        primaryStage.setTitle("Loginnn");
         primaryStage.setScene(new Scene(root, 640.0, 400));
         primaryStage.show();
     }
